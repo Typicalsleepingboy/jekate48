abf98e6424cf8e0a3a5c54e1af98fcb56cc981ed
Typicalsleepingboy.exe <99948517+Typicalsleepingboy@users.noreply.github.com>
2024-12-01 23:31:21 +0700

    Add default title

--- components/utils/NewRelease.js
+++ components/utils/NewRelease.js
@@ -1,20 +1,23 @@
 async function fetchAndDisplayMV() {
+    const defaultTitle = '[MV] Laptime Masa Remaja - JKT48 2024 Version';
+    
     try {
         const response = await fetch('https://intensprotectionexenew.vercel.app/api/video');
         const data = await response.json();
         const mvVideo = data.find(video => video.title.includes('[MV]') && !video.title.includes('BEHIND'));
         
+        const titleElement = document.getElementById('mvTitle');
+        
         if (mvVideo) {
-            const titleElement = document.getElementById('mvTitle');
             const cleanTitle = mvVideo.title.replace('[MV]', '').trim();
-            
             titleElement.textContent = cleanTitle;
-            
+        } else {
+            titleElement.textContent = defaultTitle;
         }
     } catch (error) {
         console.error('Error fetching MV data:', error);
-        document.getElementById('mvTitle').textContent = 'Error loading content';
-        document.getElementById('mvDate').textContent = '';
+        document.getElementById('mvTitle').textContent = defaultTitle;
     }
 }
+
 document.addEventListener('DOMContentLoaded', fetchAndDisplayMV);
